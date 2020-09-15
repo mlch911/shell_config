79a55a739ecf09dbe8f77a58995c5ebb6afc746e
mlch911 <mlch911@mluoc.tech>
2020-09-15 16:48:48 +0800

    Fix Bug

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ ln -s -f ~/.config/config/.tmux.conf ~/.tmux.conf
 ln -s -f ~/.config/config/.p10k.zsh ~/.p10k.zsh
 # nvim
 ln -s -f ~/.config/config/vim/init.vim ~/.config/nvim/init.vim
+ln -s -f ~/.config/config/vim/config ~/.config/nvim/config
 # coc
 ln -s -f ~/.config/config/vim/coc-settings.json  ~/.config/nvim/coc-settings.json
